3a55207af04278aa38a42cc68a077510ef57dfd9
Esteban Donis <estebandonis10@gmail.com>
2023-09-07 11:25:11 -0600

    Tierra agregada

--- src/shaders.h
+++ src/shaders.h
@@ -32,48 +32,35 @@ float random_float_in_range(float min, float max) {
 
 Fragment fragmentShader(Fragment& fragment) {
     Color color;
-    glm::vec3 redColor = glm::vec3(0.88f, 0.22f, 0.08f);
-    glm::vec3 rederColor = glm::vec3 (0.98f, 0.10f, 0.08f);
-    glm::vec3 blackColor = glm::vec3(0.2f, 0.2f, 0.2f);
-    glm::vec3 grayColor = glm::vec3(0.8f, 0.8f, 0.8f);
 
-    glm:: vec2 uv = glm::vec2(fragment.original.x, fragment.original.y);
+    glm::vec3 groundColor = glm::vec3(0.44f, 0.51f, 0.33f);
+    glm::vec3 oceanColor = glm::vec3(0.12f, 0.38f, 0.57f);
+    glm::vec3 cloudColor = glm::vec3(1.0f, 1.0f, 1.0f);
+
+    glm::vec2 uv = glm::vec2(fragment.original.x, fragment.original.y);
 
     FastNoiseLite noiseGenerator;
     noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
 
-    float ox = 900.0f;
-    float oy = 400.0f;
+    float ox = 1200.0f;
+    float oy = 3000.0f;
     float zoom = 200.0f;
 
     float noiseValue = noiseGenerator.GetNoise((uv.x + ox) * zoom, (uv.y + oy) * zoom);
 
-    glm:: vec3 tmpColor = (noiseValue < 0.3f) ? rederColor: redColor;
+    glm::vec3 tmpColor = (noiseValue < 0.5f) ? oceanColor : groundColor;
 
-    float oxc = 200.0f;
-    float oyc = 5600.0f;
-    float zoomc = 80.0f;
+    float oxc = 3400.0f;
+    float oyc = 9800.0f;
+    float zoomc = 100.0f;
 
     float noiseValueC = noiseGenerator.GetNoise((uv.x + oxc) * zoomc, (uv.y + oyc) * zoomc);
 
-    float random_float = random_float_in_range(0.2, 0.8);
-
-    if (noiseValueC > random_float) {
-        tmpColor = blackColor;
+    if (noiseValueC > 0.5f) {
+        tmpColor = cloudColor;
     }
 
-    float oxl = 200.0f;
-    float oyl = 5600.0f;
-    float random_float_L = random_float_in_range(70, 100);
-    float zooml = random_float_L;
-
-    float noiseValueL = noiseGenerator.GetNoise((uv.x + oxl) * zooml, (uv.y + oyl* zooml));
-
-    if (noiseValueL > 0.8f) {
-        tmpColor = grayColor;
-    }
-
-    color = Color (tmpColor.x, tmpColor.y, tmpColor.z);
+    color = Color(tmpColor.x, tmpColor.y, tmpColor.z);
 
     fragment.color = color * fragment.intensity;
 
